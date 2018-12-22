5515f9eb31518577ad1a771741162a88345cbbf2
Omayowfade <omomayowa.f.adebanjo@vanderbilt.edu>
2018-12-21 23:02:41 -0600

    Quick electron app; packaging error with rcedit unsolvable; removed package.json scripts

--- main.js
+++ main.js
@@ -1,50 +1,113 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+//SET ENV
+process.env.NODE_ENV = 'production';
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+let mainWindow;
+let addWindow;
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+// Listen for app to be ready
+app.on('ready', function(){
+  // Create new window
+  mainWindow = new BrowserWindow({});
+  // Load html into window
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  // Quit app when closed
+  mainWindow.on('close', function(){
+    app.quit();
   })
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+function createAddWindow(){
+ // Create new window
+ addWindow = new BrowserWindow({
+   width: 300,
+   height: 200,
+   title: 'Add Shopping List Item'
+ });
+ // Load html into window
+ addWindow.loadURL(url.format({
+   pathname: path.join(__dirname, 'addWindow.html'),
+   protocol: 'file:',
+   slashes: true
+ }));
+
+ // Garbage Collection
+ addWindow.on('close', function(){
+   addWindow = null;
+ })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+// Catch item add
+ipcMain.on('item:add', function(e, item){
+  console.log(item);
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close();
+});
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      },
+    ]
   }
-})
+];
 
-app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+// If Mac, add empty object to menu
+if(process.platform == 'darwin'){
+  // adds on to beginning of the template array
+  mainMenuTemplate.unshift({});
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      { 
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+        focusedWindow.toggleDevTools();
+      }
+    },
+    {
+      role: 'reload'
+    }
+    ]
+  });
+} 
